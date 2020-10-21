229f82da4168789f0bc045d21df79c40c597a221
hydratypelvl <hydratypelvl@gmail.com>
2020-10-21 23:26:42 +0000

    Done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+// DARBA UZDEVUMS
+// Izveidot masīvu. Masīva izmērs ir 100 elementi. Darba sākumā lietotājam tiek pieprasīts ievadīt pašreizējo izmēru N. Lietotājam jādod iespēja pašam aizpildīt masīvu. Kad programma izpilda visas darbības, lietotājam tiek piedāvāts atkārtot darbības ar citu masīva izmēru vai beigt darbu.
+
+// 2. Variants - Masīvs no N elementiem satur naturālus skaitļus. Atrast tā mazāko elementu( ja ir vairāki elementi ar tādu vērtību, tad pirmo no labās puses) un samainīt vietām ar pirmo elementu.
+
+//Darba autors - Rihards Ādminis P2-4
+
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
+// Funkcija atrod mazāko Elementu masīvā
 int findSmallestElement(int arr[], int n){
    int temp = arr[0];
    for(int i=0; i<n; i++) {
@@ -9,9 +18,10 @@ int findSmallestElement(int arr[], int n){
         temp=arr[i];
       }
    }
+   // Atgriež mazāko vērtību kā 'temp'
    return temp;
 }
-
+// Funkcija atrod mazākā elementa indeksu
 int indexofSmallestElement(int array[], int size)
 {
   int index = 0 ;
@@ -24,7 +34,13 @@ int indexofSmallestElement(int array[], int size)
       index = i ;
     }
   }
- return index;
+  // Atrgiež indeksu
+  return index;
+}
+// Funkcija apgriež masīvu otrādāk
+void reverse(int arr[], int n)
+{
+	reverse(arr, arr + n);
 }
 
 int main() {
@@ -34,55 +50,66 @@ int main() {
   string atbilde;
 
   do {
-    //Pozitiba skaitla parbaude
-  cout << "ievadiet masīva lielumu!\n";
-  do {
-    cin >> x;  
-    if(x > 0) {
+    cout << "ievadiet masīva lielumu!\n";
+    do {
+      cin >> x;  
+      //Pozitiba skaitla parbaude
+      if(x > 0) {
+        break;
+      }
+      else {
+        cout << "Ludzu ievadiet skaitli lielaku par 0!\n";
+      }
+    } while(x < 1);
+
+    cout << "Masīva garums - " << x << "\n\n";
+
+    //Masiva aizpildisana
+    for (int i = 0; i < x; i++) {
+      cout << "ievadiet skaitli " << i+1 << " - ";
+      cin >> vertibas[i];
+      // vertibas[i] = skaitlis;
     }
-    else {
-      cout << "Ludzu ievadiet skaitli lielaku par 0!\n";
+    cout << "\n";
+    for(int i = 0; i < x; i++){
+      cout << vertibas[i];
+      cout << " ";
     }
-  } while(x < 1);
+    cout << " - orģinālais";
+    //Mazakais skaitlis
+    
+    int smallest = findSmallestElement(vertibas, x);
 
-  cout << "Masīva garums - " << x << "\n\n";
+    int temparr = vertibas[0];
+    vertibas[0] = smallest;
+    // cout << smallest;
 
-  //Masiva aizpildisana
-  for (int i = 0; i < x; i++) {
-    cout << "ievadiet skaitli " << i+1 << " - ";
-    cin >> vertibas[i];
-    // vertibas[i] = skaitlis;
-  }
-  cout << "\n";
-  for(int i = 0; i < x; i++){
-    cout << vertibas[i];
-    cout << " ";
-  }
-  //Mazakais skaitlis
+    //Apgriež masīvu otrādāk
+    reverse(vertibas, x);
+    // piešķir indeksu māzākajam elementam masīvā
+    int smindex = indexofSmallestElement(vertibas, x);
+    vertibas[smindex] = temparr;
+    //Apgriež masīvu otrādāk atkārtoti
+    reverse(vertibas, x);
 
-  int smallest = findSmallestElement(vertibas, x);
-  int smindex = indexofSmallestElement(vertibas, x);
-  
+    // Izvada Pārveidoto masīvu
+    cout << "\n";
+    for(int i = 0; i < x; i++){
+      cout << vertibas[i];
+      cout << " ";
+    }
+    cout << " - apstradatais";
+    cout <<"\n\nMazaka vertiba ir - "<< smallest << "\n" << "Pozicija no labās puses - " << smindex;
 
-  int temparr = vertibas[0];
-  vertibas[0] = smallest;
-  vertibas[smindex] = temparr;
-
-  cout << "\n";
-  for(int i = 0; i < x; i++){
-    cout << vertibas[i];
-    cout << " ";
-  }
-  cout <<"\n\nMazaka vertiba ir - "<< smallest << "\n" << "Pozicija - " << smindex;
-
-  //Programmas atkartosna?
-  cout << "\nVai velaties atkartot programmu? (y/n)";
-  cin >> atbilde;
-  if(atbilde == "yes" || atbilde == "Yes"|| atbilde == "y" || atbilde == "Y" || atbilde == "ja" || atbilde == "Ja"){
-    process = true;
-  } else {
-    process = false;
-  }
+    //Programmas atkartosna?
+    cout << "\nVai velaties atkartot programmu? (y/n)";
+    cin >> atbilde;
+    if(atbilde == "yes" || atbilde == "Yes"|| atbilde == "y" || atbilde == "Y" || atbilde == "ja" || atbilde == "Ja"){
+      process = true;
+    } else {
+      process = false;
+    }
   } while(process == true);
+  // Programmas beigas
   cout << "\nProgrammas Beigas!";
 } 
